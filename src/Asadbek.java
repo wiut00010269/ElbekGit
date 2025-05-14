741f1f2d3919fbc6a47de7ce5b0bf6516f276f79
nurbek <wiut00010269@gmail.com>
2025-05-14 22:45:52 +0500

    for orqali raqamlar chiqarildi

--- src/Asadbek.java
+++ src/Asadbek.java
@@ -3,5 +3,10 @@ public class Asadbek {
     public static void main(String[] args) {
 
         System.out.println("bo boshqa branch");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
     }
 }
